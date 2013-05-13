b273cee348faf8cc8e2d017d28a82ac5c208d125
ansoncat <ansoncat@gmail.com>
2013-05-13 23:53:11 +0800

    Don't use sshconfig

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cp -f "$PWD/gitconfig" ~/.gitconfig
-cp -sf "$PWD/sshconfig" ~/.ssh/config
+#cp -sf "$PWD/sshconfig" ~/.ssh/config
 cp -sf "$PWD/bashrc" ~/.bashrc
 cp -sf "$PWD/bash_aliases" ~/.bash_aliases
