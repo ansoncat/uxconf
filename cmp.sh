0993c0dcef9c477c2fcf02ed2c6fad9c45dd8c46
ansoncat <ansoncat@gmail.com>
2017-04-06 23:08:07 +0800

    A shell for checking installed config files

---
+++ cmp.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+git diff "$PWD/gitconfig" ~/.gitconfig
+git diff "$PWD/bash_profile" ~/.bash_profile
+git diff "$PWD/bashrc" ~/.bashrc
+git diff "$PWD/bash_aliases" ~/.bash_aliases
