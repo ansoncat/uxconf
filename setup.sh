a940f059b605aa3806d7dbf52b12ec8387920770
ansoncat <ansoncat@gmail.com>
2017-04-11 16:08:57 +0800

    Remove unused sshconfig vimrc

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 cp -f "$PWD/gitconfig" ~/.gitconfig
-#cp -f "$PWD/sshconfig" ~/.ssh/config
 cp -f "$PWD/bash_profile" ~/.bash_profile
 cp -f "$PWD/bashrc" ~/.bashrc
 cp -f "$PWD/bash_aliases" ~/.bash_aliases
